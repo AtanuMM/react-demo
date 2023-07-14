6d35afcf98c8e602e2671ce392c23e87c5b3f576
Atanu Paul <atanu.paul.dev@gmail.com>
2023-07-14 12:54:48 +0530

    first commit

---
+++ src/components/demo-comp/Demo.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./demo-stlye.css"
+import Demo1 from "../demo-comp2/Demo1"
+
+
+const demo = () =>{
+    return(
+        <>
+            <h1>DEMO FILE</h1>
+            <Demo1/>
+        </>
+    )
+}
+
+export default demo
---
+++ src/components/demo-comp2/Demo1.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const demo1 = () =>{
+    return(
+        <>
+            <h2>DEMO FILE 1</h2>
+        </>
+    )
+}
+
+export default demo1
